00bf7b18b3f2a8a9f2a146fe002237d544838113
dazdraperma <42574231+dazdraperma@users.noreply.github.com>
2018-08-22 18:30:15 +0300

    added setyp.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/dazdraperma/dezirteer/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='Dezirteer',
+    version='0.1.0',
+    description='Package for dezirteer.com',
+    long_description=readme,
+    author='Vladislav Powerman',
+    author_email='vladislav.powerman@gmail.com',
+    url='https://github.com/dazdraperma/dezirteer',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
