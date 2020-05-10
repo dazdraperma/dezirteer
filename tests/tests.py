5a59e568a3f39336634078e600c35a50084eba04
dazdraperma <vladislav.powerman@gmail.com>
2020-05-09 23:45:20 -0700

    added discordance unit tests

--- tests/tests.py
+++ tests/tests.py
@@ -31,6 +31,16 @@ class TestMath(unittest.TestCase):
         g_pars_onChange = [g_filters, g_table, g_grainset, g_list_col_names]
 
 
+    def verify_values(self, n, wa, sigma, ninety_five, mswd, max_age, min_age):
+        is_n_good = (main_gui.g_number_of_good_grains[0] ==  n)
+        is_wa_good = (int(main_gui.g_number_of_good_grains[1]) == wa)
+        is_sigma_good = (round(main_gui.g_number_of_good_grains[2], 2) == sigma)
+        is_ninety_five_good = (int(main_gui.g_number_of_good_grains[3]) == ninety_five)
+        is_mswd_good = (int(main_gui.g_number_of_good_grains[4]) == mswd)
+        is_max_age_good = (main_gui.g_number_of_good_grains[5] == max_age)
+        is_min_age_good = (main_gui.g_number_of_good_grains[6] == min_age)
+        return [is_n_good, is_wa_good, is_sigma_good, is_ninety_five_good, is_mswd_good, is_max_age_good, is_min_age_good]
+
     def setUp(self):
         global g_filters, g_table, g_grainset, g_list_col_names, g_operwindow, g_pars_onChange
         self.zir = math_module.Analysis('test_zircon', 15, (0.2003, 0.0008, 0.0046), (2.082, 0.009, 0.07), 0.6, 0.6,
@@ -51,8 +61,8 @@ class TestMath(unittest.TestCase):
         global g_filters, g_table, g_grainset, g_list_col_names, g_operwindow, g_pars_onChange
 
         g_operwindow.open_and_load_file("C:/Program Files (x86)/Dezirteer/Examples/iolite_example.txt")
-        g_operwindow.entPosDiscFilt.delete(0, "end")
-        g_operwindow.entPosDiscFilt.insert(0, 4)
+        #g_operwindow.entPosDiscFilt.delete(0, "end")
+        #g_operwindow.entPosDiscFilt.insert(0, 4)
 
 
         self.set_vars()
@@ -60,6 +70,22 @@ class TestMath(unittest.TestCase):
         gui_support.onChange(6, 4, g_pars_onChange)
         gui_support.onChange(7, 10, g_pars_onChange)
         g_operwindow.clear_and_plot()
+
+        #ver_values = self.verify_values(165, 877, 0.39, 114, 22555, 3265, 404)
+        #this is the shortest code, but the most difficult to debug
+        '''for i in ver_values:
+            self.assertEqual(ver_values[i], True)
+            print (i)'''
+        # this is a longer code, but the less difficult to debug
+        '''self.assertEqual(ver_values[0], True)
+        self.assertEqual(ver_values[1], True)
+        self.assertEqual(ver_values[2], True)
+        self.assertEqual(ver_values[3], True)
+        self.assertEqual(ver_values[4], True)
+        self.assertEqual(ver_values[5], True)
+        self.assertEqual(ver_values[6], True)'''
+
+        # this is the longest and least neat code, but very easy to debug
         self.assertEqual(main_gui.g_number_of_good_grains[0], 165)
         self.assertEqual(int(main_gui.g_number_of_good_grains[1]), 877)
         self.assertEqual(round(main_gui.g_number_of_good_grains[2], 2), 0.39)
@@ -68,8 +94,8 @@ class TestMath(unittest.TestCase):
         self.assertEqual(main_gui.g_number_of_good_grains[5], 3265)
         self.assertEqual(main_gui.g_number_of_good_grains[6], 404)
 
-
         gui_support.onChange(6, 20, g_pars_onChange)
+        gui_support.onChange(7, 10, g_pars_onChange)
         g_operwindow.clear_and_plot()
         self.assertEqual(main_gui.g_number_of_good_grains[0], 173)
         self.assertEqual(int(main_gui.g_number_of_good_grains[1]), 908)
@@ -78,6 +104,18 @@ class TestMath(unittest.TestCase):
         self.assertEqual(int(main_gui.g_number_of_good_grains[4]), 23593)
         self.assertEqual(main_gui.g_number_of_good_grains[5], 3265)
         self.assertEqual(main_gui.g_number_of_good_grains[6], 404)
+
+        gui_support.onChange(6, 1, g_pars_onChange)
+        gui_support.onChange(7, 1, g_pars_onChange)
+        g_operwindow.clear_and_plot()
+        self.assertEqual(main_gui.g_number_of_good_grains[0], 67)
+        self.assertEqual(int(main_gui.g_number_of_good_grains[1]), 830)
+        self.assertEqual(round(main_gui.g_number_of_good_grains[2], 2), 0.64)
+        self.assertEqual(int(main_gui.g_number_of_good_grains[3]), 171)
+        self.assertEqual(int(main_gui.g_number_of_good_grains[4]), 18181)
+        self.assertEqual(main_gui.g_number_of_good_grains[5], 2930)
+        self.assertEqual(main_gui.g_number_of_good_grains[6], 408)
+
 
 
 
