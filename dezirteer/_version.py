62664d6172a11088d8f668801fc47748ebfe142d
dazdraperma <vladislav.powerman@gmail.com>
2023-01-08 16:05:29 -0800

    corrected release #

--- dezirteer/_version.py
+++ dezirteer/_version.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.4.b"
-__release_year__ = 2022
-__release_month__ = 12
-__release_date__ = 23
+__version__ = "1.0.4.с"
+__release_year__ = 2023
+__release_month__ = 1
+__release_date__ = 8
